f249eed1efce3c91643e6737fd499319502d7195
Kevin Jing Qiu <kevin@idempotent.ca>
2020-01-29 13:45:03 -0500

    Format

--- src/client.rs
+++ src/client.rs
@@ -1,13 +1,16 @@
-use std::time::SystemTime;
-use sha2::Sha256;
-use hmac::{Hmac, Mac};
-use base64;
 use crate::query::{Query, TextQuery};
+use base64;
+use hmac::{Hmac, Mac};
 use reqwest::blocking::Client as HttpClient;
 use reqwest::header::HeaderMap;
+use sha2::Sha256;
+use std::time::SystemTime;
 
 fn get_current_timestamp() -> u64 {
-    SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()
+    SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
 }
 
 #[derive(Debug)]
@@ -21,12 +24,23 @@ pub struct Client {
 impl Client {
     pub fn new(api_url: String, client_id: String, client_key: String) -> Self {
         let http_client = reqwest::blocking::Client::builder()
-            .http1_title_case_headers()  // because houndify API headers are case-sensitive :(
-            .build().unwrap();
-        Client{ api_url, client_id, client_key, http_client }
+            .http1_title_case_headers() // because houndify API headers are case-sensitive :(
+            .build()
+            .unwrap();
+        Client {
+            api_url,
+            client_id,
+            client_key,
+            http_client,
+        }
     }
 
-    pub fn build_auth_headers(&self, user_id: String, request_id: String, timestamp: u64) -> HeaderMap {
+    pub fn build_auth_headers(
+        &self,
+        user_id: String,
+        request_id: String,
+        timestamp: u64,
+    ) -> HeaderMap {
         let decoded_client_key = base64::decode_config(&self.client_key, base64::URL_SAFE).unwrap();
         let mut mac: Hmac<Sha256> = Hmac::new_varkey(&decoded_client_key).unwrap();
         let data = format!("{};{}{}", user_id, request_id, timestamp.to_string());
@@ -34,8 +48,16 @@ impl Client {
         let hmac_result = mac.result();
         let signature = base64::encode_config(&hmac_result.code(), base64::URL_SAFE);
         let mut header_map = HeaderMap::new();
-        header_map.insert("Hound-Client-Authentication", format!("{};{};{}", &self.client_id, &timestamp, &signature).parse().unwrap());
-        header_map.insert("Hound-Request-Authentication", format!("{};{}", &user_id, &request_id).parse().unwrap());
+        header_map.insert(
+            "Hound-Client-Authentication",
+            format!("{};{};{}", &self.client_id, &timestamp, &signature)
+                .parse()
+                .unwrap(),
+        );
+        header_map.insert(
+            "Hound-Request-Authentication",
+            format!("{};{}", &user_id, &request_id).parse().unwrap(),
+        );
         header_map
     }
 
@@ -46,10 +68,19 @@ impl Client {
 
         let user_id = "test_user";
         let request_id = "deadbeef";
-        let mut headers = self.build_auth_headers(user_id.to_string(), request_id.to_string(), timestamp);
+        let mut headers =
+            self.build_auth_headers(user_id.to_string(), request_id.to_string(), timestamp);
 
         let url = query.get_url(self.api_url.clone());
-        for (k, v) in query.get_headers(self.client_id.clone(), user_id.to_string(), request_id.to_string(), timestamp).iter() {
+        for (k, v) in query
+            .get_headers(
+                self.client_id.clone(),
+                user_id.to_string(),
+                request_id.to_string(),
+                timestamp,
+            )
+            .iter()
+        {
             headers.insert(k.clone(), v.clone());
         }
         let req = self.http_client.get(&url).headers(headers);
--- src/query.rs
+++ src/query.rs
@@ -1,10 +1,16 @@
-use url::form_urlencoded;
-use serde_json::{Value, Map, Number};
 use reqwest::header::HeaderMap;
+use serde_json::{Map, Number, Value};
+use url::form_urlencoded;
 
 pub trait Query {
     fn get_url(&self, api_url: String) -> String;
-    fn get_headers(&self, client_id: String, user_id: String, request_id: String, timestamp: u64) -> HeaderMap;
+    fn get_headers(
+        &self,
+        client_id: String,
+        user_id: String,
+        request_id: String,
+        timestamp: u64,
+    ) -> HeaderMap;
 }
 
 #[derive(Debug)]
@@ -18,26 +24,40 @@ pub struct AuthInfo {
 pub struct TextQuery {
     user_id: String,
     query_id: String,
-    pub query: String,  // TODO: remove pub
+    pub query: String, // TODO: remove pub
 }
 
 impl TextQuery {
     pub fn new(query_text: String) -> TextQuery {
-        TextQuery{query: query_text, user_id: "test_user".to_string(), query_id: "deadbeef".to_string()}
+        TextQuery {
+            query: query_text,
+            user_id: "test_user".to_string(),
+            query_id: "deadbeef".to_string(),
+        }
     }
 }
 
 impl Query for TextQuery {
     fn get_url(&self, api_url: String) -> String {
-        let url: String = form_urlencoded::Serializer::new(format!("{}v1/text?", api_url.to_string()))
-            .append_pair("query", &self.query)
-            .finish();
+        let url: String =
+            form_urlencoded::Serializer::new(format!("{}v1/text?", api_url.to_string()))
+                .append_pair("query", &self.query)
+                .finish();
         url
     }
 
-    fn get_headers(&self, client_id: String, user_id: String, request_id: String, timestamp: u64) -> HeaderMap {
+    fn get_headers(
+        &self,
+        client_id: String,
+        user_id: String,
+        request_id: String,
+        timestamp: u64,
+    ) -> HeaderMap {
         let mut request_info = Map::new();
-        request_info.insert("TimeStamp".to_string(), Value::Number(Number::from(timestamp)));
+        request_info.insert(
+            "TimeStamp".to_string(),
+            Value::Number(Number::from(timestamp)),
+        );
         request_info.insert("ClientID".to_string(), Value::String(client_id.to_string()));
         request_info.insert("UserID".to_string(), Value::String(user_id.to_string()));
         request_info.insert("SDK".to_string(), Value::String("rust".to_string()));
@@ -48,7 +68,10 @@ impl Query for TextQuery {
 
         let mut header_map = HeaderMap::new();
         header_map.insert("Hound-Request-Info", request_info_json.parse().unwrap());
-        header_map.insert("Hound-Request-Info-Length", request_info_json.len().to_string().parse().unwrap());
+        header_map.insert(
+            "Hound-Request-Info-Length",
+            request_info_json.len().to_string().parse().unwrap(),
+        );
 
         header_map
     }
