8c86526a869cd4b25ba03cd59f2019436696f7b2
Kevin Jing Qiu <kevin@idempotent.ca>
2020-01-29 23:43:29 -0500

    Fix test code

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,9 @@ mod tests {
         let client_key = String::from("jLTVjUOFBSetQtA3l-lGlb75rPVqKmH_JFgOVZjl4BdJqOq7PwUpub8ROcNnXUTssqd6M_7rC8Jn3_FjITouxQ==");
         let api_base = String::from("https://api.houndify.com/");
 
-        let c = Client::new(api_base, client_id, client_key);
+        let c = Client::new(&api_base, &client_id, &client_key);
         // let q = query::TextQuery::new(String::from("what is 1+1?"));
         let q = "what is 1 + 1?".to_string();
-        c.text_query(q);
+        c.text_query(&q);
     }
 }
