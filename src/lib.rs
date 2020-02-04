fcfed0083b6189aaa24d696e08db79172eef3a58
Kevin Jing Qiu <kevin@idempotent.ca>
2020-02-03 21:59:44 -0500

    Hound server response parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub use crate::client::{Client, Result};
 pub use crate::error::{HoundifyError, InvalidRequestInfoError};
 pub use crate::query::{RequestInfo, TextQuery, VoiceQuery};
+pub use crate::response::HoundServerResponse;
 
 mod client;
 mod error;
 mod query;
+mod response;
--- src/response.rs
+++ src/response.rs
@@ -1,38 +1,42 @@
-use serde_json::{Map, String, Value}
+use serde::{Deserialize, Serialize};
+use serde_json;
+use serde_json::{Map, Value};
 
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all = "PascalCase")]
 pub struct HoundServerResponse {
-    format: str,
-    format_version: str,
-    status: str,
-    error_message: Option<str>,
+    format: String,
+    format_version: String,
+    status: String,
+    error_message: Option<String>,
     num_to_return: Option<i64>,
-    all_results: Option<Map<String, Value>>,
+    all_results: Option<Vec<Map<String, Value>>>,
     disambiguation: Option<Map<String, Value>>,
     results_are_final: Option<Vec<Value>>,
     domain_usage: Option<Vec<Value>>,
     build_info: Option<Map<String, Value>>,
-    query_id: str,
-    server_generated_id: Option<str>,
+    #[serde(rename = "QueryID")]
+    query_id: String,
+    server_generated_id: Option<String>,
     audio_length: Option<f64>,
     real_speech_time: Option<f64>,
     cpu_speech_time: Option<f64>,
     real_time: Option<f64>,
     cpu_time: Option<f64>,
-    local_or_remote: Option<str>,
-    local_or_remote_reason: Option<str>,
+    local_or_remote: Option<String>,
+    local_or_remote_reason: Option<String>,
 }
 
 #[cfg(test)]
 mod response_tests {
     use super::*;
+    use serde_json;
 
     #[test]
     fn test_hound_server_response_parse() {
-        let file = File::open("tests/sample_result.json").unwrap();
+        let file = std::fs::File::open("tests/sample_result.json").unwrap();
         let hsr: HoundServerResponse = serde_json::from_reader(file).unwrap();
-        println!("{}", hsr);
+        println!("{:#?}", hsr);
         assert_eq!(1, 1);
     }
 }
